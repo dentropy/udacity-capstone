1ffa8d50c048b6e8e718c60684b7d534883a8c56
Paul Mullins <paulmullins@protonmail.com>
2022-04-07 21:15:48 -0400

    Added a script that to clone some test repos

---
+++ data/clone-test-repos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir repos
+cd repos
+git clone https://github.com/cardano-foundation/CIPs.git
+git clone https://github.com/bitcoin/bips.git
+git clone https://github.com/ethereum/EIPs.git
+
